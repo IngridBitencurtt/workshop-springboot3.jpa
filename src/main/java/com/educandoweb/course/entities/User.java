e13f25b9efd1b359855193532ff951fd6fe333f2
ingrid.fernandes <ingrid.fernandes@sensedia.com>
2023-04-20 09:11:35 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,73 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+    private static final long serialVersionUID = 1l;
+
+    private long id;
+    private String name;
+    private String phone;
+    private String password;
+
+
+    public User() {
+    }
+
+    public User(long id, String name, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public User setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public User setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
